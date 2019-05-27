b3554ddfb98986ac924c36286c06dccb9069e182
ligz <ligz@office.polyv.net lgz@pV0>
2019-05-27 16:01:40 +0800

    还原

--- src/main/java/com/sinmn/iweb/auth/controller/AuthUserController.java
+++ src/main/java/com/sinmn/iweb/auth/controller/AuthUserController.java
@@ -7,8 +7,6 @@ import com.sinmn.iweb.auth.model.AuthUser;
 import com.sinmn.iweb.auth.resource.AuthResource;
 import com.sinmn.iweb.auth.service.AuthUserService;
 import com.sinmn.iweb.auth.vo.inVO.AuthLoginInVO;
-import com.sinmn.iweb.auth.vo.inVO.AuthUserRenameInVO;
-import com.sinmn.iweb.auth.vo.inVO.AuthUserRenewInVO;
 import com.sinmn.iweb.auth.vo.inVO.AuthUserRepasswdInVO;
 import com.sinmn.iweb.auth.vo.searchVO.AuthUserSearchVO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,69 +23,46 @@ import javax.servlet.http.HttpServletRequest;
 public class AuthUserController {
 
 
-
-	@Autowired
-	private AuthUserService authUserService;
+    @Autowired
+    private AuthUserService authUserService;
 
 
     @Value("${sysUrl}")
     private String sysUrl;
 
 
-	@RequestMapping(path ="/admin/auth/common/authUser/login.do",method = {RequestMethod.POST})
-	public ApiResult<Object> login(@RequestBody AuthLoginInVO authLoginInVO,HttpServletRequest req)
-			throws CommonException
-	{
-		return ApiResult.getSuccess("登录成功",authUserService.login(authLoginInVO,req));
-	}
-	
-	@RequestMapping(path ="/admin/auth/instance/authUser/list.do",method = {RequestMethod.POST})
-	@AuthResource(parent="用户列表",name="列表搜索")
-	public ApiResult<Object> list(@RequestBody AuthUserSearchVO svo)
-			throws CommonException
-	{
-		return ApiResult.getSuccess(authUserService.list(svo,AuthContext.getUserInfoInnerVO()));
-	}
-	
-	@RequestMapping(path ="/admin/auth/instance/authUser/save.do",method = {RequestMethod.POST})
-	@AuthResource(parent="用户列表",name="新增保存",type=AuthResource.BUTTON)
-	public ApiResult<Object> save(@RequestBody AuthUser authUser)
-			throws CommonException
-	{
-		return ApiResult.getSuccess(authUserService.save(authUser,AuthContext.getUserInfoInnerVO()));
-	}
-	
-	@RequestMapping(path ="/admin/auth/instance/authApp/rePasswd.do",method = {RequestMethod.POST})
-	public ApiResult<Object> rePasswd(@RequestBody AuthUserRepasswdInVO authUserRepasswdInVO)
-			throws CommonException
-	{
-		return ApiResult.getSuccess(authUserService.rePasswd(authUserRepasswdInVO,AuthContext.getUserInfoInnerVO()));
-	}
-
-    @RequestMapping(path ="/admin/auth/instance/authApp/reUserName.do",method = {RequestMethod.POST})
-    public ApiResult<Object> reUserName(@RequestBody AuthUserRenameInVO authUserRenameInVO)
-            throws CommonException
-    {
-        return ApiResult.getSuccess(authUserService.reName(authUserRenameInVO,AuthContext.getUserInfoInnerVO()));
+    @RequestMapping(path = "/admin/auth/common/authUser/login.do", method = {RequestMethod.POST})
+    public ApiResult<Object> login(@RequestBody AuthLoginInVO authLoginInVO, HttpServletRequest req)
+            throws CommonException {
+        return ApiResult.getSuccess("登录成功", authUserService.login(authLoginInVO, req));
     }
 
-    //更新用户名和密码
-    @RequestMapping(path ="/admin/auth/instance/authApp/renew.do",method = {RequestMethod.POST})
-    public ApiResult<Object> renew(@RequestBody AuthUserRenewInVO authUserRenewInVO)
-            throws CommonException
-    {
-        return ApiResult.getSuccess(authUserService.reNew(authUserRenewInVO,AuthContext.getUserInfoInnerVO()));
+    @RequestMapping(path = "/admin/auth/instance/authUser/list.do", method = {RequestMethod.POST})
+    @AuthResource(parent = "用户列表", name = "列表搜索")
+    public ApiResult<Object> list(@RequestBody AuthUserSearchVO svo)
+            throws CommonException {
+        return ApiResult.getSuccess(authUserService.list(svo, AuthContext.getUserInfoInnerVO()));
     }
-	
-	@RequestMapping(path ="/admin/auth/instance/authUser/active.do",method = {RequestMethod.POST})
-	@AuthResource(parent="用户列表",name="账号激活",type=AuthResource.BUTTON)
-	public ApiResult<Object> active(@RequestBody AuthUser authUser)
-			throws CommonException
-	{
-		return ApiResult.getSuccess(authUserService.active(authUser,AuthContext.getUserInfoInnerVO()));
-	}
+
+    @RequestMapping(path = "/admin/auth/instance/authUser/save.do", method = {RequestMethod.POST})
+    @AuthResource(parent = "用户列表", name = "新增保存", type = AuthResource.BUTTON)
+    public ApiResult<Object> save(@RequestBody AuthUser authUser)
+            throws CommonException {
+        return ApiResult.getSuccess(authUserService.save(authUser, AuthContext.getUserInfoInnerVO()));
+    }
+
+    @RequestMapping(path = "/admin/auth/instance/authApp/rePasswd.do", method = {RequestMethod.POST})
+    public ApiResult<Object> rePasswd(@RequestBody AuthUserRepasswdInVO authUserRepasswdInVO)
+            throws CommonException {
+        return ApiResult.getSuccess(authUserService.rePasswd(authUserRepasswdInVO, AuthContext.getUserInfoInnerVO()));
+    }
+
+    @RequestMapping(path = "/admin/auth/instance/authUser/active.do", method = {RequestMethod.POST})
+    @AuthResource(parent = "用户列表", name = "账号激活", type = AuthResource.BUTTON)
+    public ApiResult<Object> active(@RequestBody AuthUser authUser)
+            throws CommonException {
+        return ApiResult.getSuccess(authUserService.active(authUser, AuthContext.getUserInfoInnerVO()));
+    }
 
 
-
-	
 }
